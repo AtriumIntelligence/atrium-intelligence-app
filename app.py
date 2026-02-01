4dedd84cfac8fa5f4eb2b89fae8bd2e3fbef6433
Jamison Welch <AtriumIntelligence@Outlook.com>
2026-02-01 16:44:11 -0500

    Logo Fix

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.set_page_config(
 col1, col2 = st.columns([1, 3])
 
 with col1:
-    st.image("assets/atrium_logo.png", use_container_width=True)
+    st.image("assets/AtriumIntelligenceLogo.png", use_container_width=True)
 
 with col2:
     st.title("Atrium Intelligence")
